1c7d283ff5c8fe163c1ac542ccac304e60a8f117
gyd-Doggy <pcdoggy@qq.com>
2017-12-23 14:19:36 +0800

    new Web API sln
    new Web API sln.

---
+++ hello.WebAPI.V1/hello.WebAPI.V1/Controllers/MainController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace hello.WebAPI.V1.Controllers
+{
+    public class MainController : ApiController
+    {
+        // API请求入口
+        [HttpPost]
+        public async void DatasInterface(dynamic obj)
+        {
+            try
+            {
+                var streamProvider = await Request.Content.ReadAsMultipartAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ hello.WebAPI.V1/hello.WebAPI.V1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hello.WebAPI.V1.WebApiApplication" Language="C#" %>
