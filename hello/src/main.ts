152744e137a6d7835e9ce9500632d95adb0d0270
gyd-Doggy <pcdoggy@qq.com>
2017-12-28 16:59:39 +0800

    delete backups
    delete backups

--- hello/src/main.ts
+++ hello/src/main.ts
@@ -4,8 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// Enable production mode unless running locally
-if (!/localhost/.test(document.location.host)) {
+if (environment.production) {
   enableProdMode();
 }
 
