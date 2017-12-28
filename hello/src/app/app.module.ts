effd03b204c8d3336f662b30eb804c49cc86f961
gyd-Doggy <pcdoggy@qq.com>
2017-12-28 15:28:13 +0800

    release files
    backups commit.

--- hello/src/app/app.module.ts
+++ hello/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
 
+const appRoutes: Routes = [
+  { path: 'books', component: BooksComponent },
+
+  { path: '', redirectTo: '/books', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
